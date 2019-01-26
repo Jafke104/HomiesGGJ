3c7f45b2577443c6c6e0fc9f97ee6d645f2238aa
DESKTOP-24TES1G\coolkath <coolkath23@gmail.com>
2019-01-25 23:31:06 -0800

    Player Movement Scripts

---
+++ HomiesGGJ/Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    [Range(1, 10)]
+    public float jumpVelocity;
+
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+    Rigidbody2D rb;
+
+    void Awake(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update(){
+
+        if(Input.GetButtonDown ("Jump"))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpVelocity; 
+        }
+
+
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+}
---
+++ HomiesGGJ/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float Speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float Dirx = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+
+        transform.position = new Vector2(transform.position.x + Dirx, transform.position.y);
+
+    }
+}
