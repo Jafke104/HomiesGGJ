7f1a6ee3d149bbc87f4b41b49af7f95faf7d211a
Elton Dan <roxas190@gmail.com>
2019-01-26 00:30:56 -0800

    UI Functionality update
    Added Script to pause game and set menu active.

---
+++ HomiesGGJ/Assets/Scripts/UI/PauseFunctions.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseFunctions : MonoBehaviour
+{
+    private bool GamePaused;
+    [SerializeField]
+    private GameObject PauseMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GamePaused = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (GamePaused) {
+                UnpauseGame();
+            } else {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame() {
+        Debug.Log("Game is paused");
+        GamePaused = true;
+        Time.timeScale = 0;
+        PauseMenu.SetActive(true);
+    }
+
+    public void UnpauseGame() {
+        Debug.Log("Game is unpaused");
+        GamePaused = false;
+        Time.timeScale = 1;
+        PauseMenu.SetActive(false);
+    }
+}
