fdc381749ba5e4572e9f9b0a52acbdc8795be750
Elton Dan <roxas190@gmail.com>
2019-01-27 15:08:05 -0800

    GameManager
    updated

--- HomiesGGJ/Assets/Scripts/GameManager.cs
+++ HomiesGGJ/Assets/Scripts/GameManager.cs
@@ -2,14 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+[System.Serializable]
+public class GameManager {
+    public static GameManager current;
     public List<int> choices;
-
-    // TESTING ONLY
-    private void Start() {
-        newGame();
-        choices.Add(0);
-    }
 
     public void newGame() {
         choices = new List<int>();
@@ -17,5 +13,9 @@ public class GameManager : MonoBehaviour {
 
     public void addChoices(int choice) {
         choices.Add(choice);
+    }
+
+    public List<int> GetChoices() {
+        return choices;
     }
 }
