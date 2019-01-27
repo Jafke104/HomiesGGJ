8e6d490c3c88ab2ebb8e133adca69705095c46e3
Elton Dan <roxas190@gmail.com>
2019-01-26 20:10:32 -0800

    Fixed errors

--- HomiesGGJ/Assets/Scripts/GameManager.cs
+++ HomiesGGJ/Assets/Scripts/GameManager.cs
@@ -3,5 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+    private List<int> choices;
 
+    public void newGame() {
+        choices = new List<int>();
+    }
+
+    public void addChoices(int choice) {
+        choices.Add(choice);
+    }
 }
