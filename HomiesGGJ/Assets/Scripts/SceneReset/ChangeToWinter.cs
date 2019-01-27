47c0fa48855202c4a12902fb742cd5fbe3f886f6
Marshall Nguyen <marshahn@uci.edu>
2019-01-27 13:39:48 -0800

    wrote script changing scripts from spring to fall and fall to winter

---
+++ HomiesGGJ/Assets/Scripts/SceneReset/ChangeToWinter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManager;
+
+public class ChangeToWinter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("WinterSeason");
+        }
+    }
+}
