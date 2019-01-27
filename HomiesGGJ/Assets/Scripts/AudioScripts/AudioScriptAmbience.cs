5446bfa0e848bb712ac702ca12c711709e190c21
DESKTOP-24TES1G\coolkath <coolkath23@gmail.com>
2019-01-27 12:02:59 -0800

    Music Scripts

---
+++ HomiesGGJ/Assets/Scripts/AudioScripts/AudioScriptAmbience.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScriptAmbience : MonoBehaviour
+{
+
+    public AudioClip MusicClip;
+
+    public AudioSource MusicSource;
+
+    void Start()
+    {
+        MusicSource.clip = MusicClip;
+        MusicSource.volume = 0.3f;
+        MusicSource.Play();
+    }
+}
---
+++ HomiesGGJ/Assets/Scripts/AudioScripts/AudioScriptMusic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScriptMusic : MonoBehaviour
+{
+
+    public AudioClip MusicClip;
+
+    public AudioSource MusicSource;
+
+    void Start()
+    {
+        MusicSource.clip = MusicClip;
+        MusicSource.volume = 1.5f;
+        MusicSource.Play();
+    }
+}
